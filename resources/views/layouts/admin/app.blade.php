82eb98dd25e74e2a352de3d9388dc363b5a70c28
bosa-hub <bosa.amorah@gmail.com>
2026-02-27 14:31:13 +0300

    Add Content-Security-Policy meta tag

--- resources/views/layouts/admin/app.blade.php
+++ resources/views/layouts/admin/app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="_token" content="{{ csrf_token() }}">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
     <meta name="robots" content="nofollow, noindex">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
